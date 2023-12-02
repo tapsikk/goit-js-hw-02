80c53389e244be40bd63b1415e15eb5d13e83fc0
tapsikk <tapsgame@gmail.com>
2023-12-02 22:14:47 +0200

    upd

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,6 @@
 function checkForSpam(message){
-  const checkSpam = message.toLowerCase();
-if(checkSpam.includes("spam") || checkSpam.includes("sale")){
-  return true;
-} else {
-  return false;
-}
+const checkSpam = message.toLowerCase();
+return checkSpam.includes("spam") || checkSpam.includes("sale")
 }
 
 console.log(checkForSpam("Latest technology news")); // false
